f2bd9cc824a9e694be57b959d0400255f833f333
Kai Tamkun <ktamkun@ucsc.edu>
2019-05-18 21:38:35 -0700

    More colors and styles

--- ansi.h
+++ ansi.h
@@ -5,11 +5,16 @@
 #include <map>
 #include <string>
 
+using std::string;
+
 namespace ansi {
-	enum color {RED, ORANGE, YELLOW, GREEN, BLUE, CYAN, MAGENTA, PURPLE, BLACK, GRAY, LIGHTGRAY, WHITE};
+	enum color {
+		RED, ORANGE, YELLOW, YEEN, GREEN, BLUE, CYAN, MAGENTA, PURPLE, BLACK, GRAY, LIGHTGRAY, WHITE, PINK, SKY
+	};
+
+	enum style {BOLD, DIM, ITALIC, UNDERLINE};
 
 	class ansistream {
-
 		private:
 			std::ostream &out;
 			ansi::color color;
@@ -17,22 +22,38 @@ namespace ansi {
 		public:
 			ansistream(std::ostream &out_): out(out_) { }
 			ansistream & operator<<(const ansi::color c);
-			ansistream & operator<<(const std::string &str);
+			ansistream & operator<<(const string &str);
 	};
 
-	const std::string reset_fg = "\x1b[39m";
-	const std::map<color, std::string> colors = {
-		{ORANGE,    "\x1b[38;5;202m"},
-		{BLACK,     "\x1b[30m"},
-		{RED,       "\x1b[31m"},
-		{GREEN,     "\x1b[32m"},
-		{YELLOW,    "\x1b[33m"},
-		{BLUE,      "\x1b[34m"},
-		{MAGENTA,   "\x1b[35m"},
-		{CYAN,      "\x1b[36m"},
-		{WHITE,     "\x1b[37m"},
-		{LIGHTGRAY, "\x1b[38;5;7m"},
-		{GRAY,      "\x1b[38;5;8m"},
+	const string reset        = "\e[0m";
+	const string reset_fg     = "\e[39m";
+	const string reset_bg     = "\e[49m";
+	const string reset_bold   = "\e[22m";
+	const string reset_dim    = "\e[22m";
+	const string reset_italic = "\e[23m";
+	const string reset_under  = "\e[24m";
+
+	const string bold   = "\e[1m";
+	const string dim    = "\e[2m";
+	const string italic = "\e[3m";
+	const string under  = "\e[4m";
+
+	const std::map<color, string> colors = {
+		{RED,       "\e[31m"},
+		{ORANGE,    "\e[38;5;202m"},
+		{YELLOW,    "\e[33m"},
+		{YEEN,      "\e[38;5;112m"},
+		{GREEN,     "\e[32m"},
+		{BLUE,      "\e[34m"},
+		{CYAN,      "\e[36m"},
+		{MAGENTA,   "\e[35m"},
+		{PURPLE,    "\e[38;5;56m"},
+		{BLACK,     "\e[30m"},
+		{GRAY,      "\e[38;5;8m"},
+		{LIGHTGRAY, "\e[38;5;7m"},
+		{WHITE,     "\e[37m"},
+		{PINK,      "\e[38;5;219m"},
+		{SKY,       "\e[38;5;153m"},
 	};
 }
 
