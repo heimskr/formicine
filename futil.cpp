c35a1df85587b0f1d5ffc4d46285a3d2242d2faa
Kai Tamkun <ktamkun@ucsc.edu>
2019-10-15 12:27:48 -0700

    Moved a lot of functions from spjalla::util

--- futil.cpp
+++ futil.cpp
@@ -1,7 +1,7 @@
 #include "futil.h"
 
-namespace formicine {
-	std::vector<std::string> util::split(const std::string &str, const std::string &delimeter, bool condense) {
+namespace formicine::util {
+	std::vector<std::string> split(const std::string &str, const std::string &delimeter, bool condense) {
 		if (str.empty())
 			return {};
 
@@ -26,15 +26,15 @@ namespace formicine {
 		return out;
 	}
 
-	std::string util::filter(const std::string &str, const std::string &allowed_chars) {
+	std::string filter(const std::string &str, const std::string &allowed_chars) {
 		return filter(str, [&](char ch) { return allowed_chars.find(ch) != std::string::npos; });
 	}
 
-	std::string util::antifilter(const std::string &str, const std::string &forbidden_chars) {
+	std::string antifilter(const std::string &str, const std::string &forbidden_chars) {
 		return filter(str, [&](char ch) { return forbidden_chars.find(ch) == std::string::npos; });
 	}
 
-	std::string util::lower(std::string str) {
+	std::string lower(std::string str) {
 		// TODO: Unicode. Of course.
 		for (size_t i = 0, length = str.length(); i < length; ++i) {
 			if ('A' <= str[i] && str[i] <= 'Z')
@@ -44,7 +44,7 @@ namespace formicine {
 		return str;
 	}
 
-	std::string util::upper(std::string str) {
+	std::string upper(std::string str) {
 		// TODO: Unicode. Of course.
 		for (size_t i = 0, length = str.length(); i < length; ++i) {
 			if ('a' <= str[i] && str[i] <= 'z')
@@ -54,7 +54,7 @@ namespace formicine {
 		return str;
 	}
 
-	std::string util::nth_word(const std::string &str, size_t n, bool condense) {
+	std::string nth_word(const std::string &str, size_t n, bool condense) {
 		if (condense) {
 			const std::vector<std::string> words = split(str, " ", true);
 			return n < words.size()? words[n] : "";
@@ -69,7 +69,7 @@ namespace formicine {
 		return str.substr(index + 1, nth_index(str, ' ', n + 1) - index - 1);
 	}
 
-	size_t util::word_count(const std::string &str, bool condense) {
+	size_t word_count(const std::string &str, bool condense) {
 		size_t count = 0, i = 0, length = str.length();
 		if (length == 0)
 			return 0;
@@ -94,5 +94,192 @@ namespace formicine {
 		}
 
 		return count + 1;
+	}
+
+	std::string & trim(std::string &str) {
+		return ltrim(rtrim(str));
+	}
+
+	std::string trim(const std::string &str) {
+		std::string copy {str};
+		return trim(copy);
+	}
+
+	std::string & ltrim(std::string &str) {
+		if (str.empty())
+			return str;
+
+		const size_t first = str.find_first_not_of(" \t");
+		if (first == std::string::npos)
+			str.clear();
+		else
+			str.erase(0, first);
+
+		return str;
+	}
+
+	std::string ltrim(const std::string &str) {
+		std::string copy {str};
+		return ltrim(copy);
+	}
+
+	std::string & rtrim(std::string &str) {
+		if (str.empty())
+			return str;
+
+		char back = str.back();
+		while (back == ' ' || back == '\t') {
+			str.pop_back();
+			back = str.back();
+		}
+
+		return str;
+	}
+
+	std::string rtrim(const std::string &str) {
+		std::string copy {str};
+		return rtrim(copy);
+	}
+
+	std::pair<ssize_t, ssize_t> word_indices(const std::string &str, size_t cursor) {
+		const size_t length = str.length();
+
+		ssize_t word_index = -1;
+		ssize_t sub_index  = -1;
+		char prev_char = '\0';
+
+		if (str.empty())
+			return {-1, -1};
+
+		if (cursor == 0)
+			return str[0] == ' '? std::pair(-1, -1) : std::pair(0, 0);
+
+		for (size_t i = 0; i < length; ++i) {
+			char ch = str[i];
+
+			if (ch != ' ' && (prev_char == '\0' || prev_char == ' ')) {
+				++word_index;
+				sub_index = 0;
+			}
+
+			if (ch == ' ') {
+				if (prev_char != ' ') {
+					// We've reached the end of a word. If this is where the cursor is, we're done.
+					// Otherwise, it's time to increment the word index.
+					if (i == cursor)
+						return {word_index, sub_index};
+				} else if (i == cursor) {
+					// If we're within a group of spaces and this is the where the cursor is, return a negative number
+					// because that means the cursor isn't in a word.
+					return {-word_index - 2, -1};
+				}
+			} else {
+				if (i == cursor)
+					return {word_index, sub_index};
+
+				++sub_index;
+			}
+
+			prev_char = ch;
+		}
+
+		if (cursor == length && prev_char != ' ')
+			return {word_index, sub_index};
+
+		return {-word_index - 2, -1};
+	}
+
+	size_t index_of_word(const std::string &str, size_t n) {
+		const size_t length = str.length();
+		size_t word_index = 0;
+
+		char prev_char = '\0';
+		for (size_t i = 0; i < length; ++i) {
+			char ch = str[i];
+
+			if (ch == ' ' && (prev_char != ' ' && prev_char != '\0')) {
+				// We're at a space and the previous character wasn't a space (or the beginning of the string).
+				// This means we've just left a word, so increment the word index.
+				++word_index;
+			}
+
+			if (ch != ' ' && (prev_char == ' ' || prev_char == '\0') && word_index == n)
+				return i;
+
+			prev_char = ch;
+		}
+
+		return length;
+	}
+
+	std::string skip_words(const std::string &str, size_t n) {
+		return str.substr(index_of_word(str, n));
+	}
+
+	size_t last_index_of_word(const std::string &str, size_t n) {
+		const size_t length = str.length();
+		size_t word_index = 0;
+
+		char prev_char = '\0';
+		for (size_t i = 0; i < length; ++i) {
+			char ch = str[i];
+
+			if (ch == ' ' && (prev_char != ' ' && prev_char != '\0')) {
+				// We're at a space and the previous character wasn't a space (or the beginning of the string).
+				// This means we've just left a word, so check whether we're at the requested word index.
+				if (word_index++ == n)
+					return i;
+			}
+
+			prev_char = ch;
+		}
+
+		return length;
+	}
+
+	ssize_t replace_word(std::string &str, size_t n, const std::string &word) {
+		const size_t index = index_of_word(str, n);
+		const size_t original_length = str.length();
+		if (index == original_length)
+			return -1;
+
+		size_t original_word_width = 0;
+		for (size_t i = index; i < original_length; ++i) {
+			if (std::isspace(str[i]))
+				break;
+			++original_word_width;
+		}
+
+		str.replace(index, original_word_width, word);
+		return index + word.length();
+	}
+
+	std::string & remove_suffix(std::string &word, const std::string &suffix) {
+		if (word.empty() || suffix.empty())
+			return word;
+
+		const size_t pos = word.rfind(suffix);
+		if (pos != std::string::npos && pos + suffix.length() == word.length())
+			word.erase(pos);
+
+		return word;
+	}
+
+	std::string remove_suffix(const std::string &word, const std::string &suffix) {
+		std::string word_copy {word};
+		remove_suffix(word_copy, suffix);
+		return word_copy;
+	}
+
+	bool parse_long(const std::string &str, long &out) {
+		const char *c_str = str.c_str();
+		char *end;
+		long parsed = strtol(c_str, &end, 10);
+		if (end == c_str + str.length()) {
+			out = parsed;
+			return true;
+		}
+
+		return false;
 	}
 }
--- futil.h
+++ futil.h
@@ -5,67 +5,139 @@
 #include <string>
 #include <vector>
 
-namespace formicine {
-	struct util {
-		/** Splits a string by a given delimiter. If condense is true, empty strings won't be included in the output. */
-		static std::vector<std::string> split(const std::string &str, const std::string &delimeter,
-			bool condense = true);
+namespace formicine::util {
+	/** Splits a string by a given delimiter. If condense is true, empty strings won't be included in the output. */
+	std::vector<std::string> split(const std::string &str, const std::string &delimeter, bool condense = true);
 
-		template <typename Iter>
-		static std::string join(Iter begin, Iter end, const std::string &delim = " ") {
-			if (begin == end)
-				return "";
+	template <typename Iter>
+	std::string join(Iter begin, Iter end, const std::string &delim = " ") {
+		if (begin == end)
+			return "";
 
-			std::ostringstream oss;
-			bool first = true;
-			while (begin != end) {
-				if (!first) {
-					oss << delim;
-				} else {
-					first = false;
-				}
-
-				oss << *begin;
-				++begin;
+		std::ostringstream oss;
+		bool first = true;
+		while (begin != end) {
+			if (!first) {
+				oss << delim;
+			} else {
+				first = false;
 			}
 
-			return oss.str();
+			oss << *begin;
+			++begin;
 		}
 
-		template <typename Container>
-		static std::string join(const Container &cont, const std::string &delim = " ") {
-			return join(cont.begin(), cont.end(), delim);
+		return oss.str();
+	}
+
+	template <typename Container>
+	std::string join(const Container &cont, const std::string &delim = " ") {
+		return join(cont.begin(), cont.end(), delim);
+	}
+
+	template <typename Pred, typename std::enable_if<std::is_invocable<Pred, char>::value>::type * = nullptr>
+	std::string filter(const std::string &str, Pred predicate) {
+		std::string out;
+		out.reserve(str.size());
+		for (char ch: str) {
+			if (predicate(ch))
+				out.push_back(ch);
 		}
 
-		template <typename Pred, typename std::enable_if<std::is_invocable<Pred, char>::value>::type * = nullptr>
-		static std::string filter(const std::string &str, Pred predicate) {
-			std::string out;
-			out.reserve(str.size());
-			for (char ch: str) {
-				if (predicate(ch))
-					out.push_back(ch);
+		return out;
+	}
+
+	std::string filter(const std::string &str, const std::string &allowed_chars);
+	std::string antifilter(const std::string &str, const std::string &allowed_chars);
+
+	std::string lower(std::string);
+	std::string upper(std::string);
+
+	template <typename T>
+	size_t nth_index(const std::string &str, const T &to_find, int n) {
+		size_t index = 0;
+		for (int i = 0; i < n; ++i)
+			index = str.find(to_find, i? index + 1 : i);
+		return index;
+	}
+
+	std::string nth_word(const std::string &, size_t, bool condense = false);
+	size_t word_count(const std::string &, bool condense = true);
+
+	/** Trims spaces and tabs from both ends of a string. */
+	std::string & trim(std::string &);
+	/** Trims spaces and tabs from both ends of a string. */
+	std::string   trim(const std::string &);
+
+	/** Trims spaces and tabs from the start of a string. */
+	std::string & ltrim(std::string &);
+	/** Trims spaces and tabs from the start of a string. */
+	std::string   ltrim(const std::string &);
+
+	/** Trims spaces and tabs from the end of a string. */
+	std::string & rtrim(std::string &);
+	/** Trims spaces and tabs from the end of a string. */
+	std::string   rtrim(const std::string &);
+
+	/** Returns the index of the word that a given index is in in addition to the index within the word.
+	 *  If the cursor is within a group of multiple spaces between two words, the first value will be negative.
+	 *  If the first value is -1, the cursor is before the first word. -2 indicates that the cursor is before the
+	 *  second word, -3 before the third, and so on. The second value will be -1 if the first value is negative. */
+	std::pair<ssize_t, ssize_t> word_indices(const std::string &, size_t);
+
+	/** Returns the index of the first character in the n-th word of a string. If n is greater than the number of
+	 *  words in the string, the length of the string is returned. */
+	size_t index_of_word(const std::string &, size_t n);
+
+	/** Returns a substring that begins at the n-th word of a string. */
+	std::string skip_words(const std::string &, size_t n = 1);
+
+	/** Returns the index of first character after the n-th word of a string. If n is greater than the number of
+	 *  words in the string, the length of the string is returned. */
+	size_t last_index_of_word(const std::string &, size_t n);
+
+	/** Replaces the n-th word of a string with another string and returns the position of the first character
+	 *  after the replacement word (or -1 if the string has no n-th word). */
+	ssize_t replace_word(std::string &, size_t n, const std::string &);
+
+	/** Removes a suffix from a word if one is present and returns a reference to the now-modified string. */
+	std::string & remove_suffix(std::string &word, const std::string &suffix);
+
+	/** Removes a suffix from a word if one is present. */
+	std::string remove_suffix(const std::string &word, const std::string &suffix);
+
+	/** Attempts to parse a string as a long. If the string is a valid representation of a long, the parsed long is
+	 *  stored in the given long reference and the function returns true. Otherwise, the function just returns
+	 *  false. */
+	bool parse_long(const std::string &, long &);
+
+	/** Returns a vector of all elements in a range that begin with a given string. */
+	template <typename T, typename Iter>
+	std::vector<T> starts_with(Iter start, Iter end, const std::string &prefix) {
+		std::vector<T> out {};
+		std::copy_if(start, end, std::back_inserter(out), [&](const std::string &str) {
+			return str.find(prefix) == 0;
+		});
+		return out;
+	}
+
+	/** Treats a range like a circular buffer and finds the next value after a given value. If the given value isn't
+	 *  present in the range, the function returns the value at the beginning of the range. If the range is empty,
+	 *  the function throws std::invalid_argument. */
+	template <typename Iter>
+	std::string & next_in_sequence(Iter begin, Iter end, const std::string &str) {
+		if (begin == end)
+			throw std::invalid_argument("Empty string");
+
+		for (Iter iter = begin; iter != end; ++iter) {
+			if (*iter == str) {
+				++iter;
+				return iter == end? *begin : *iter;
 			}
-
-			return out;
 		}
 
-		static std::string filter(const std::string &str, const std::string &allowed_chars);
-		static std::string antifilter(const std::string &str, const std::string &allowed_chars);
-
-		static std::string lower(std::string);
-		static std::string upper(std::string);
-
-		template <typename T>
-		static size_t nth_index(const std::string &str, const T &to_find, int n) {
-			size_t index = 0;
-			for (int i = 0; i < n; ++i)
-				index = str.find(to_find, i? index + 1 : i);
-			return index;
-		}
-
-		static std::string nth_word(const std::string &, size_t, bool condense = false);
-		static size_t word_count(const std::string &, bool condense = true);
-	};
+		return *begin;
+	}
 }
 
 #endif
